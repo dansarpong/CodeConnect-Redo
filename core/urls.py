d3c87daa49e205ca6a274ba6b385dda89473fc18
dansarpong <dansarpong.me@gmail.com>
2024-07-23 12:55:44 +0000

    Update for dashboard view

--- core/urls.py
+++ core/urls.py
@@ -1,16 +1,17 @@
 from django.contrib import admin
 from django.urls import path, include
-from users.views import login, register, profile, CustomPasswordResetView, CustomPasswordResetDoneView, CustomPasswordResetConfirmView, CustomPasswordResetCompleteView
+import users.views
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', login, name='login'),
-    path('register/', register, name='register'),
-    path('profile/', profile, name='profile'),
-    path('password_reset/', CustomPasswordResetView.as_view(), name='password_reset'),
-    path('password_reset/done/', CustomPasswordResetDoneView.as_view(), name='password_reset_done'),
-    path('reset/<uidb64>/<token>/', CustomPasswordResetConfirmView.as_view(), name='password_reset_confirm'),
-    path('reset/done/', CustomPasswordResetCompleteView.as_view(), name='password_reset_complete'),
+    path('', users.views.login_user, name='login'),
+    path('register/', users.views.register, name='register'),
+    path('dashboard/', users.views.dashboard, name='dashboard'),
+    path('profile/', users.views.profile, name='profile'),
+    path('password_reset/', users.views.CustomPasswordResetView.as_view(), name='password_reset'),
+    path('password_reset/done/', users.views.CustomPasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', users.views.CustomPasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset/done/', users.views.CustomPasswordResetCompleteView.as_view(), name='password_reset_complete'),
     path('accounts/', include('django.contrib.auth.urls')),
 ]
